dca8a75444026b66273c7ecae0e5d8d5ab6062d1
Ben Biederman <benbiederman@gmail.com>
2021-11-26 10:17:54 -0600

    Add eat data for local's guide

---
+++ js/lgEat.js
@@ -0,0 +1,105 @@
+class lgReview {
+    constructor(name, img, src, rating, seachTerms) {
+        this.name = name;
+        this.img = img;
+        this.src = src;
+        this.rating = rating;
+        this.searchTerms = seachTerms;
+    }
+}
+
+const _1919Kitchen = new lgReview(`1919 Kitchen & Tap`, '../img/locals-guide/eat/lunch/1919-kitchen-and-tap.jpg', '../locals-guide/1919-kitchen-and-tap.html', 3.5, ["lunch", "dinner", "1919 Kitchen and Tap", "1919 kitchen & tap", "green bay"]);
+
+const aldosPizza = new lgReview(`Aldo's Pizza`, '../img/locals-guide/eat/lunch/aldos-pizza.jpg', '../locals-guide/aldos-pizza.html', 3.9, ["dinner", "pizza", "aldos pizza", "aldo's pizza", "green bay"]);
+
+const alsHamburger = new lgReview(`Al's Hamburger`, '../img/locals-guide/eat/lunch/als-hamburger', '../locals-guide/als-hamburger.html', 4.25, ["lunch", "dinner", "burgers", "als hamburger", "al's hamburger", "green bay"]);
+
+const anduzzis = new lgReview(`Anduzzi's`, '../img/locals-guide/eat/lunch/anduzzis.jpg', '../locals-guide/anduzzis.html', 4.4, ["lunch", "dinner", "green bay", "howard", "anduzzis", `anduizz's`]);
+
+const angelina = new lgReview(`Angelina`, '../img/locals-guide/eat/lunch/angelina.jpg', '../locals-guide/angelina.html', 4.75, ["dinner", "green bay", "Italian", "angelina", "angelinas"]);
+
+const basils = new lgReview(`Basil's II`, '../img/locals-guide/eat/lunch/basils-ii.jpg', '../locals-guide/basils-ii.html', 4.4, ["lunch", "dinner", "denmark", "basils", `basil's 2`, "basils 2", `basil's ii`]);
+
+const bayFamily = new lgReview(`Bay Family Restaurant`, '../img/locals-guide/eat/breakfast/bay-family-restaurant.jpg', '../locals-guide/bay-family-restaurant.html', 4.0, ["breakfast", "lunch", "dinner", "green bay", "bay family restaurant"]);
+
+const blackHoney = new lgReview(`Black Honey Hashery`, '../img/locals-guide/eat/breakfast/black-honey-hashery.jpg', '../locals-guide/bay-family-restaurant.html', 4.75, ["breakfast", "lunch", "de pere", "black honey hashery"]);
+
+const blackSheep = new lgReview(`Black Sheep Pub & Grill`, '../img/locals-guide/eat/lunch/black-sheep-pub-and-grill.jpg', '../locals-guide/basils-ii.html', 4.25, ["lunch", "dinner", "green bay", "black sheep pub and grill", "black sheep pub & grill"]);
+
+const brickhouseBurgers = new lgReview(`Brickhouse Craft Burgers`, '../img/locals-guide/eat/lunch/brickhouse-craft-burgers.jpg', '../locals-guide/brickhouse-craft-burgers.html', 4.0, ["lunch", "dinner", "de pere", "brickhouse burgers", "brick house"]);
+
+const cheeseCakeHeaven = new lgReview(`Cheese Cake Heaven`, '../img/locals-guide/eat/lunch/cheese-cake-heaven.jpg', '../locals-guide/cheese-cake-heaven.html', 4.0, ["breakfast", "lunch", "dinner", "dessert", "green bay", "cheesecake heaven", "cheese cake heaven"]);
+
+const chinaKitchen = new lgReview(`China Kitchen`, '../img/locals-guide/eat/lunch/china-kitchen.jpg', '../locals-guide/china-kitchen.html', 4.25, ["lunch", "dinner", "chinese", "green bay", "china kitchen"]);
+
+const copperState = new lgReview(`Copper State Brewing Co.`, '../img/locals-guide/eat/lunch/copper-state-brewing-burger.jpg', '../locals-guide/copper-state-brewing.html', 4.4, ["lunch", "dinner", "green bay", "copperstate brewing", "copper state brewing"]);
+
+const cozumel = new lgReview(`Cozumel`, '../img/locals-guide/eat/lunch/cozumel.jpg', '../locals-guide/cozumel.html', 4.3, ["lunch", "dinner", "green bay", "cozumel", "mexican"]);
+
+const driftInn = new lgReview(`Drift Inn`, '../img/locals-guide/eat/lunch/drift-inn.jpg', '../locals-guide/drift-inn.html', 4.25, ["lunch", "dinner", "green bay", "the drift inn", "drift inn"]);
+
+const elMaya = new lgReview(`El Maya`, '../img/locals-guide/eat/lunch/el-maya.jpg', '../locals-guide/el-maya.html', 3.75, ["lunch", "dinner", "de pere", "mexican", "el maya"]);
+
+const elSarape = new lgReview(`El Sarape`, '../img/locals-guide/eat/lunch/el-sarape.jpg', '../locals-guide/el-sarape.html', 4.4, ["lunch", "dinner", "green bay", "mexican", "el sarape"]);
+
+const fourWayBar = new lgReview(`Four Way Bar`, '../img/locals-guide/eat/lunch/four-way-bar.jpg', '../locals-guide/four-way-bar.html', 3.5, ["lunch", "dinner", "suamico", "four-way bar", "four way bar"]);
+
+const glassNickelPizza = new lgReview(`Glass Nickel Pizza`, '../img/locals-guide/eat/lunch/glass-nickel-pizza.jpg', '../locals-guide/glass-nickel-pizza.html', 4.0, ["lunch", "dinner", "green bay", "glass nickel pizza", "pizza"]);
+
+const graystoneAleHouse = new lgReview(`Graystone Ale House`, '../img/locals-guide/eat/lunch/graystone-ale-house.jpg', '../locals-guide/graystone-ale-house.html', 3.75, ["lunch", "dinner", "de pere", "graystone ale house"]);
+
+const greenBayDistillery = new lgReview(`Green Bay Distillery`, '../img/locals-guide/eat/lunch/green-bay-distillery.jpg', '../locals-guide/green-bay-distillery.html', 4.25, ["lunch", "dinner", "green bay", "green bay distillery", "distillery"]);
+
+const greenTeaChinese = new lgReview(`Green Tea Chinese`, '../img/locals-guide/eat/lunch/green-tea-chinese.jpg', '../locals-guide/green-tea-chinese.html', 4.3, ["lunch", "dinner", "green bay", "green tea chinese", "chinese"]);
+
+const hagemeisterPark = new lgReview(`Hagemeister Park`, '../img/locals-guide/eat/lunch/hagemeister-park.jpg', '../locals-guide/hagemeister-park.html', 4.3, ["lunch", "dinner", "green bay", "hagemeister park"]);
+
+const happyJoes = new lgReview(`Happy Joe's Pizza`, '../img/locals-guide/eat/lunch/happy-joes.jpg', '../locals-guide/happy-joes.html', 3.75, ["lunch", "dinner", "green bay", "happy joe's pizza", "pizza"]);
+
+const heartlandPizza = new lgReview(`Heartland Pizza`, '../img/locals-guide/eat/lunch/heartland-pizza.jpg', '../locals-guide/heartland-pizza.html', 4.75, ["lunch", "dinner", "green bay", "heartland pizza", "pizza"]);
+
+const hillTopCafe = new lgReview(`Hill Top Cafe`, '../img/locals-guide/eat/lunch/hilltop-cafe.jpg', '../locals-guide/hill-top-cafe.html', 4.4, ["lunch", "dinner", "green bay", "hill top cafe", "hilltop cafe"]);
+
+const jakesPizza = new lgReview(`Jake's Pizza`, '../img/locals-guide/eat/lunch/jakes-pizza.jpg', '../locals-guide/jakes-pizza.html', 3.75, ["dinner", "green bay", "jakes pizza", "jake's pizza", "pizza"]);
+
+const juliesCafe = new lgReview(`Julie's Cafe`, '../img/locals-guide/eat/breakfast/julies-cafe.jpg', '../locals-guide/julies-cafe.html', 4.3, ["breakfast", "lunch", "green bay", "de pere", "julie's cafe", "julies cafe"]);
+
+const krollsWest = new lgReview(`Kroll's West`, '../img/locals-guide/eat/lunch/krolls-west.jpg', '../locals-guide/krolls-west.html', 3.75, ["lunch", "green bay", "krolls west", "kroll's west"]);
+
+const legendLarrys = new lgReview(`Legend Larry's`, '../img/locals-guide/eat/lunch/legend-larrys.jpg', '../locals-guide/legend-larrys.html', 4.75, ["lunch", "dinner", "green bay", "legend larrys", "legend larry's", "wings"]);
+
+const luigis = new lgReview(`Luigi's Pizza Palace`, '../img/locals-guide/eat/lunch/luigis-pizza.jpg', '../locals-guide/luigis-pizza-palace.html', 4.4, ["lunch", "dinner", "green bay", "suamico", "luigis pizza palace", "luigi's pizza palace", "pizza"]);
+
+const macs = new lgReview(`Macs`, '../img/locals-guide/eat/lunch/macs-mac-n-cheese-shop.jpg', '../locals-guide/macs.html', 4.5, ["lunch", "dinner", "green bay", "macaroni and cheese shop", "macs"]);
+
+const narrowBridge = new lgReview(`Narrow Bridge`, '../img/locals-guide/eat/lunch/narrow-bridge-brewhouse.jpg', '../locals-guide/narrow-bridge.html', 4.25, ["lunch", "dinner", "green bay", "narrow bridge"]);
+
+const nicoletCafe = new lgReview(`Nicolet Cafe`, '../img/locals-guide/eat/breakfast/nicolet-cafe.jpg', '../locals-guide/nicolet-cafe.html', 3.75, ["breakfast", "lunch", "dinner", "de pere", "nicolet cafe"]);
+
+const notByBreadAlone = new lgReview(`Not By Bread Alone`, '../img/locals-guide/eat/lunch/not-by-bread-alone.jpg', '../locals-guide/not-by-bread-alone.html', 4.4, ["lunch", "green bay"]);
+
+const oldMexico = new lgReview(`Old Mexico`, '../img/locals-guide/eat/lunch/old-mexico.jpg', '../locals-guide/old-mexico.html', 4.25, ["lunch", "dinner", "mexican", "suamico"]);
+
+const rAndDs = new lgReview(`R & D's House Divided`, '../img/locals-guide/eat/lunch/r-and-d-house-divided.jpg', '../locals-guide/r-&-d-house-divided.html', 3.0, ["lunch", "dinner", "green bay", "r and ds house divided", "r and d's house divided", "r & ds house divided", "r & d's house divided"]);
+
+const rustique = new lgReview(`Rustique Pizzeria`, '../img/locals-guide/eat/lunch/rustique-pizzeria.jpg', '../locals-guide/rustique-pizzeria.html', 4.75, ["lunch", "dinner", "suamico", "rustique pizza", "rustique pizzeria", "pizza"]);
+
+const sammysPizza = new lgReview(`Sammy's Pizza`, '../img/locals-guide/eat/lunch/sammys-pizza.jpg', '../locals-guide/sammys-pizza.html', 4.3, ["lunch", "dinner", "green bay", "sammys pizza", "sammy's pizza", "pizza"]);
+
+const sarasArtisanGelato = new lgReview(`Sara's Artisan Gelato`, '../img/locals-guide/eat/dessert/saras-artisan-gelato.jpg', '../locals-guide/saras-artisan-gelato.html', 4.75, ["dessert", "green bay", "gelato", "ice cream"]);
+
+const sgambatis = new lgReview(`Sgambati's`, '../img/locals-guide/eat/lunch/sgambatis.jpg', '../locals-guide/sgambatis.html', 4.3, ["lunch", "dinner", "de pere", "sgambatis", "sgambati's", "sgambati's new york pizza", "pizza"]);
+
+const smartCow = new lgReview(`Smart Cow`, '../img/locals-guide/eat/dessert/smart-cow.jpg', '../locals-guide/smart-cow.html', 4.0, ["dessert", "green bay", "smart cow", "ice cream", "frozen yogurt"]);
+
+const stadiumView = new lgReview(`Stadium View`, '../img/locals-guide/eat/lunch/stadium-view.jpg', '../locals-guide/stadium-view.html', 4.25, ["lunch", "dinner", "green bay", "stadium view"]);
+
+const tBaconsBbq = new lgReview(`T. Bacon's BBQ`, '../img/locals-guide/eat/lunch/t-bacons-bbq.jpg', '../locals-guide/t-bacons-bbq.html', 4.0, ["lunch", "dinner", "green bay", "t bacon's bbq", "t. bacon's bbq", "t bacons bbq", "t. bacons bbq", "barbeque"]);
+
+const theAbbey = new lgReview(`The Abbey`, '../img/locals-guide/eat/lunch/the-abbey.jpg', '../locals-guide/the-abbey.html', 4.1, ["lunch", "dinner", "de pere", "the abbey"]);
+
+const theBar = new lgReview(`The Bar`, '../img/locals-guide/eat/lunch/the-bar.jpg', '../locals-guide/the-bar.html', 4.0, ["lunch", "dinner", "green bay", "the bar"]);
+
+const thePancakePlace = new lgReview(`The Pancake Place`, '../img/locals-guide/eat/breakfast/the-pancake-place.jpg', '../locals-guide/the-pancake-place.html', 4.6, ["breakfast", "lunch", "green bay", "the pancake place", "pancake place"]);
+
+const zestys = new lgReview(`Zesty's`, '../img/locals-guide/eat/lunch/zestys.jpg', '../locals-guide/zestys.html', 4.4, ["lunch", "dinner", "dessert", "howard", "suamico", "allouez", "green bay", "zestys", "zesty's"]);
