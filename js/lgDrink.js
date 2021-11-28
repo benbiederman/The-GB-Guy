e44bfc12cce3558077657a37965ab0e24ad1d92d
Ben Biederman <benbiederman@gmail.com>
2021-11-27 18:44:51 -0600

    Add drink data for local's guide.

---
+++ js/lgDrink.js
@@ -0,0 +1,128 @@
+// Local's Guide Data
+class lgReview {
+    constructor(name, img, src, imgDesc, rating, seachTerms) {
+        this.name = name;
+        this.img = img;
+        this.imgDesc = imgDesc;
+        this.src = src;
+        this.rating = rating;
+        this.searchTerms = seachTerms;
+    }
+}
+
+const _1919Kitchen = new lgReview(`1919 Kitchen & Tap`, '../img/locals-guide/eat/lunch/1919-kitchen-and-tap.jpg', `1919 Kitchen and Tap burger with fries`,'../locals-guide/1919-kitchen-and-tap.html', 3.5, ["alcohol", "1919 Kitchen and Tap", "1919 kitchen & tap", "green bay"]);
+
+const anduzzis = new lgReview(`Anduzzi's`, '../img/locals-guide/eat/lunch/anduzzis.jpg', `Anduzzi's mac n cheese burger with fries`, '../locals-guide/anduzzis.html', 4.4, ["alcohol", "green bay", "howard", "anduzzis", `anduizz's`]);
+
+const basils = new lgReview(`Basil's II`, '../img/locals-guide/eat/lunch/basils-ii.jpg', `Basil's II butter burger`, '../locals-guide/basils-ii.html', 4.4, ["alcohol", "denmark", "basils", `basil's 2`, "basils 2", `basil's ii`]);
+
+const blackHoney = new lgReview(`Black Honey Hashery`, '../img/locals-guide/eat/breakfast/black-honey-hashery.jpg', `Black Honey Hashery biscuits and gravy`, '../locals-guide/bay-family-restaurant.html', 4.75, ["coffee", "de pere", "black honey hashery"]);
+
+const blackSheep = new lgReview(`Black Sheep Pub & Grill`, '../img/locals-guide/eat/lunch/black-sheep-pub-and-grill.jpg', `Black Sheep burger and fries`, '../locals-guide/basils-ii.html', 4.25, ["alcohol", "green bay", "black sheep pub and grill", "black sheep pub & grill"]);
+
+const brickhouseBurgers = new lgReview(`Brickhouse Craft Burgers`, '../img/locals-guide/eat/lunch/brickhouse-craft-burgers.jpg', `Brickhouse cheese burger with fries`, '../locals-guide/brickhouse-craft-burgers.html', 4.0, ["alcohol", "de pere", "brickhouse burgers", "brick house"]);
+
+const copperState = new lgReview(`Copper State Brewing Co.`, '../img/locals-guide/eat/lunch/copper-state-brewing-burger.jpg', `Copper State Brewing macaroni and cheese burger with fries`, '../locals-guide/copper-state-brewing.html', 4.4, ["alcohol", "coffee", "green bay", "copperstate brewing", "copper state brewing"]);
+
+const cozumel = new lgReview(`Cozumel`, '../img/locals-guide/eat/lunch/cozumel.jpg', `Cozumel enchiladas`, '../locals-guide/cozumel.html', 4.3, ["alcohol", "green bay", "cozumel", "mexican"]);
+
+const driftInn = new lgReview(`Drift Inn`, '../img/locals-guide/eat/lunch/drift-inn.jpg', `Drift Inn butter burger`, '../locals-guide/drift-inn.html', 4.25, ["alcohol", "green bay", "the drift inn", "drift inn"]);
+
+const elMaya = new lgReview(`El Maya`, '../img/locals-guide/eat/lunch/el-maya.jpg', `El Maya enchiladas`, '../locals-guide/el-maya.html', 3.75, ["alcohol", "de pere", "mexican", "el maya"]);
+
+const elSarape = new lgReview(`El Sarape`, '../img/locals-guide/eat/lunch/el-sarape.jpg', `El Sarape burrito`, '../locals-guide/el-sarape.html', 4.4, ["alcohol", "green bay", "mexican", "el sarape"]);
+
+const fourWayBar = new lgReview(`Four Way Bar`, '../img/locals-guide/eat/lunch/four-way-bar.jpg', `Four Way Bar's wings and fries`, '../locals-guide/four-way-bar.html', 3.5, ["alcohol", "suamico", "four-way bar", "four way bar"]);
+
+const glassNickelPizza = new lgReview(`Glass Nickel Pizza`, '../img/locals-guide/eat/lunch/glass-nickel-pizza.jpg', `Glass Nicke Pizza pepperoni pizza`, '../locals-guide/glass-nickel-pizza.html', 4.0, ["alcohol", "green bay", "glass nickel pizza", "pizza"]);
+
+const graystoneAleHouse = new lgReview(`Graystone Ale House`, '../img/locals-guide/eat/lunch/graystone-ale-house.jpg', `Graystone Ale House burger and tator tots`, '../locals-guide/graystone-ale-house.html', 3.75, ["alcohol", "de pere", "graystone ale house"]);
+
+const greenBayDistillery = new lgReview(`Green Bay Distillery`, '../img/locals-guide/eat/lunch/green-bay-distillery.jpg', `Green Bay Distillery curd burger`, '../locals-guide/green-bay-distillery.html', 4.25, ["alcohol", "green bay", "green bay distillery", "distillery"]);
+
+const hagemeisterPark = new lgReview(`Hagemeister Park`, '../img/locals-guide/eat/lunch/hagemeister-park.jpg', `Hagemeister Park chicken carbonara`, '../locals-guide/hagemeister-park.html', 4.3, ["alcohol", "green bay", "hagemeister park"]);
+
+const heartlandPizza = new lgReview(`Heartland Pizza`, '../img/locals-guide/eat/lunch/heartland-pizza.jpg', `Heartland Pizza's pepperoni pizza`, '../locals-guide/heartland-pizza.html', 4.75, ["alcohol", "green bay", "heartland pizza", "pizza"]);
+
+const krollsWest = new lgReview(`Kroll's West`, '../img/locals-guide/eat/lunch/krolls-west.jpg', `Kroll's cheesecurds`, '../locals-guide/krolls-west.html', 3.75, ["alcohol", "krolls west", "kroll's west"]);
+
+const legendLarrys = new lgReview(`Legend Larry's`, '../img/locals-guide/eat/lunch/legend-larrys.jpg', `Legend Larry's DOA wings`, '../locals-guide/legend-larrys.html', 4.75, ["alcohol", "green bay", "legend larrys", "legend larry's", "wings"]);
+
+const luigis = new lgReview(`Luigi's Pizza Palace`, '../img/locals-guide/eat/lunch/luigis-pizza.jpg', `Luigi's Pizza Palace pepperoni pizza`, '../locals-guide/luigis-pizza-palace.html', 4.4, ["alcohol", "green bay", "suamico", "luigis pizza palace", "luigi's pizza palace", "pizza"]);
+
+const lunaCoffee = new lgReview(`Luna Coffee Roasters`, '../img/locals-guide/drink/coffee/luna-coffee-roasters.jpg', `Luna Coffee Roasters store front`, '../locals-guide/luna-coffee-roasters.html', 4.75, ["coffee", "luna coffee roasters", "de pere"]);
+
+const narrowBridge = new lgReview(`Narrow Bridge`, '../img/locals-guide/eat/lunch/narrow-bridge-brewhouse.jpg', `Narrow Bridge burger and fries`, '../locals-guide/narrow-bridge.html', 4.25, ["alcohol", "green bay", "narrow bridge"]);
+
+const nicoletCafe = new lgReview(`Nicolet Cafe`, '../img/locals-guide/eat/breakfast/nicolet-cafe.jpg', `Nicolet Cafe french toast and hashbrowns`, '../locals-guide/nicolet-cafe.html', 3.75, ["coffee", "de pere", "nicolet cafe"]);
+
+const notByBreadAlone = new lgReview(`Not By Bread Alone`, '../img/locals-guide/eat/lunch/not-by-bread-alone.jpg', `Not By Bread Alone turkey sandwich`, '../locals-guide/not-by-bread-alone.html', 4.4, ["coffee", "green bay"]);
+
+const oldMexico = new lgReview(`Old Mexico`, '../img/locals-guide/eat/lunch/old-mexico.jpg', `Old Mexico enchiladas`, '../locals-guide/old-mexico.html', 4.25, ["alcohol", "mexican", "suamico"]);
+
+const rAndDs = new lgReview(`R & D's House Divided`, '../img/locals-guide/eat/lunch/r-and-d-house-divided.jpg', `R & D's Italian Beef`, '../locals-guide/r-&-d-house-divided.html', 3.0, ["alcohol", "green bay", "r and ds house divided", "r and d's house divided", "r & ds house divided", "r & d's house divided"]);
+
+const rustique = new lgReview(`Rustique Pizzeria`, '../img/locals-guide/eat/lunch/rustique-pizzeria.jpg', `Rustique's pepperoni and mac n cheese pizza`, '../locals-guide/rustique-pizzeria.html', 4.75, ["alcohol", "suamico", "rustique pizza", "rustique pizzeria", "pizza"]);
+
+const sammysPizza = new lgReview(`Sammy's Pizza`, '../img/locals-guide/eat/lunch/sammys-pizza.jpg', `Sammy's pepperoni pizza`, '../locals-guide/sammys-pizza.html', 4.3, ["alcohol", "green bay", "sammys pizza", "sammy's pizza", "pizza"]);
+
+const sgambatis = new lgReview(`Sgambati's`, '../img/locals-guide/eat/lunch/sgambatis.jpg', `Sgambati's New York Style pepperoni pizza`, '../locals-guide/sgambatis.html', 4.3, ["alcohol", "de pere", "sgambatis", "sgambati's", "sgambati's new york pizza", "pizza"]);
+
+const stadiumView = new lgReview(`Stadium View`, '../img/locals-guide/eat/lunch/stadium-view.jpg', `Stadium View's french dip sandwich`, '../locals-guide/stadium-view.html', 4.25, ["alcohol", "green bay", "stadium view"]);
+
+const stirUpsBar = new lgReview(`Stir-Ups Bar`, '../img/locals-guide/drink/alcohol/stir-ups.jpg', `Stir-Ups Bar dance floor`,'../locals-guide/stir-ups-bar.html', 3.75, ["alcohol", "stirups bar", "stir ups bar", "green bay"]);
+
+const theAbbey = new lgReview(`The Abbey`, '../img/locals-guide/eat/lunch/the-abbey.jpg', `The Abbey buffalo chicken wrap with fries`, '../locals-guide/the-abbey.html', 4.1, ["alcohol", "de pere", "the abbey"]);
+
+const theBar = new lgReview(`The Bar`, '../img/locals-guide/eat/lunch/the-bar.jpg', `The Bar's buffalo wings`, '../locals-guide/the-bar.html', 4.0, ["alcohol", "green bay", "the bar"]);
+
+const theExchange = new lgReview(`The Exchange`, '../img/locals-guide/drink/coffee/the-exchange.jpg', `The Exchange coffee flight`,'../locals-guide/the-exchange.html', 4.15, ["coffee", "the exchange", "de pere"]);
+
+const items = [_1919Kitchen, anduzzis, basils, blackHoney, blackSheep, brickhouseBurgers, copperState, cozumel, driftInn, elMaya, elSarape, fourWayBar, glassNickelPizza, graystoneAleHouse, greenBayDistillery, hagemeisterPark, heartlandPizza, krollsWest, legendLarrys, luigis, lunaCoffee, narrowBridge, nicoletCafe, notByBreadAlone, oldMexico, rAndDs, rustique, sammysPizza, sgambatis, stadiumView, stirUpsBar, theAbbey, theBar, theExchange]
+
+// Filter items list by rating and create content item for each item
+function createLocalsGuide(){
+    const localsGuide = items.sort((a, b) => b.rating - a.rating);
+
+    localsGuide.map((item) => {
+        createContentItem(item);
+    })
+
+}
+
+createLocalsGuide()
+
+// Create content item
+function createContentItem(content){
+    const contentContainer = document.querySelector('.content-container');
+
+    const contentItem = document.createElement('article');
+    contentItem.className = 'content-item';
+    contentContainer.appendChild(contentItem);
+
+    contentItem.addEventListener('click', () => {
+        window.location.href = content.src;
+    })
+
+    const contentFigure = document.createElement('figure');
+    contentItem.appendChild(contentFigure);
+
+    const contentImg = document.createElement('img');
+    contentImg.src = content.img;
+    contentImg.alt = `${content.name} menu item`;
+    contentFigure.appendChild(contentImg);
+
+    const contentHeader = document.createElement('h2');
+    contentHeader.textContent = content.name;
+    contentItem.appendChild(contentHeader);
+
+    if(content.rating){
+        const contentRating = document.createElement('p');
+        contentRating.textContent = `${content.rating} / 5.0`;
+        contentItem.appendChild(contentRating);
+    }
+
+    const contentLink = document.createElement('p');
+    contentLink.textContent = '- Read More -';
+    contentItem.appendChild(contentLink);
+}
