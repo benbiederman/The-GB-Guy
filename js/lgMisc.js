15ad2361bd7c8ab2cafc406a1334c49b0481186a
Ben Biederman <benbiederman@gmail.com>
2021-12-02 07:13:29 -0600

    Add search functionality to misc.html

--- js/lgMisc.js
+++ js/lgMisc.js
@@ -1,3 +1,10 @@
+const searchInput = document.querySelector('.search-input');
+const serachBtn = document.querySelector('.search-btn');
+const contentContainer = document.querySelector('.content-container');
+const xBtn = document.querySelector('.x-btn');
+const searchBtn = document.querySelector('.search-btn');
+
+
 // Local's Guide Data
 class lgReview {
     constructor(name, img, imgDesc, src, searchTerms) {
@@ -17,25 +24,30 @@ const parkingGuide = new lgReview(`Lambeau Field Parking Guide`, '../img/locals-
 
 const tailgatingGuide = new lgReview(`Lambeau Field Tailgating Guide`, '../img/locals-guide/misc/lambeau-field-tailgating.jpg', `Grill with hot coals`, '../locals-guide/lambeau-field-tailgating-guide.html', ["packers tailgating", "tailgating", "lambeau field tailgating"]);
 
-const miscItems = [ticketGuide, hotelGuide, parkingGuide, tailgatingGuide];
+const content = [ticketGuide, hotelGuide, parkingGuide, tailgatingGuide];
 
-// Map through all items to create content item
-miscItems.map((item) => {
-    createContentItem(item);
-})
+// Generate initial content
+createLocalsGuide(content)
+
+
+// Creates content
+function createLocalsGuide(arr){
+
+    let localsGuide = content;
+
+    // Generate content by filtered list
+    localsGuide.map((item) => {
+        createContentItem(item);
+    })
+
+}
 
 // Create content item
 function createContentItem(content){
-    const miscContainer = document.querySelector('.misc-container');
-
     const contentItem = document.createElement('article');
     contentItem.className = 'content-item';
-    if(content.rating) {
-        goToContainer.appendChild(contentItem);
-    } else {
-        miscContainer.appendChild(contentItem);
-    }
-    
+    contentContainer.appendChild(contentItem);
+
     contentItem.addEventListener('click', () => {
         window.location.href = content.src;
     })
@@ -45,7 +57,7 @@ function createContentItem(content){
 
     const contentImg = document.createElement('img');
     contentImg.src = content.img;
-    contentImg.alt = content.imgDesc;
+    contentImg.alt = `${content.name} menu item`;
     contentFigure.appendChild(contentImg);
 
     const contentHeader = document.createElement('h2');
@@ -62,3 +74,73 @@ function createContentItem(content){
     contentLink.textContent = '- Read More -';
     contentItem.appendChild(contentLink);
 }
+
+// Create Null Search Results
+function noResults(){
+    const noResultsSection = document.createElement('div');
+    noResultsSection.className = 'null-search';
+    contentContainer.appendChild(noResultsSection);
+
+    const header = document.createElement('p');
+    header.textContent = "Ope.";
+    noResultsSection.appendChild(header);
+
+    const p1 = document.createElement('p');
+    p1.textContent = "Your search results came back about as empty as the trophy case in U.S. Bank Stadium.";
+    noResultsSection.appendChild(p1);
+
+    const p2 = document.createElement('p');
+    p2.textContent = "Please adjust your search";
+    noResultsSection.appendChild(p2);
+
+}
+
+// Search Function
+searchInput.addEventListener('input', (e) => {
+    let search = e.target.value;
+    let searchResults = [];
+    xBtn.style.display = 'block';
+
+    if(search.length == 0){
+        contentContainer.innerHTML = '';
+        searchResults = content;
+        createLocalsGuide(searchResults)
+        xBtn.style.display = 'none';
+    } else {
+        content.map((c) => {
+            for(let i = 0; i < c.searchTerms.length; i++ ){
+                if(c.searchTerms[i].toLowerCase().includes(search.toLowerCase())){
+                    searchResults = [...searchResults, {c}.c];
+                }
+            }
+        })
+    }
+
+    // Filter duplicates in array
+    const uniqueResults = [...searchResults.reduce((map, obj) => map.set(obj.name, obj) , new Map()).values()];
+
+    // Check if results, if no result product no result / else create results
+    if( uniqueResults.length === 0 && search.length !== 0) {
+        contentContainer.innerHTML = '';
+        noResults()
+    } else {
+        contentContainer.innerHTML = '';
+        for(let i = 0; i < uniqueResults.length; i++){
+            createContentItem(uniqueResults[i])
+        }
+    }
+
+})
+
+// Prevent refresh upon Go button click
+searchBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+})
+
+xBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    searchInput.value = '';
+    contentContainer.innerHTML = '';
+    xBtn.style.display = 'none';
+    createLocalsGuide(content);
+})
