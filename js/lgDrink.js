0e5605da77908aa1b9f3823e90c5f9732d96b6e9
Ben Biederman <benbiederman@gmail.com>
2021-12-20 08:04:54 -0600

    Add Kavarna Coffeehouse to local's guide.

--- js/lgDrink.js
+++ js/lgDrink.js
@@ -23,13 +23,13 @@ const anduzzis = new lgReview(`Anduzzi's`, '../img/locals-guide/eat/lunch/anduzz
 
 const basils = new lgReview(`Basil's II`, '../img/locals-guide/eat/lunch/basils-ii.jpg', `Basil's II butter burger`, '../locals-guide/basils-ii.html', 4.4, ["alcohol", "denmark", "basils", `basil's 2`, "basils 2", `basil's ii`]);
 
-const blackHoney = new lgReview(`Black Honey Hashery`, '../img/locals-guide/eat/breakfast/black-honey-hashery.jpg', `Black Honey Hashery biscuits and gravy`, '../locals-guide/bay-family-restaurant.html', 4.75, ["coffee", "de pere", "black honey hashery"]);
+const blackHoney = new lgReview(`Black Honey Hashery`, '../img/locals-guide/drink/coffee/black-honey-hashery.jpg', `Black Honey Hashery biscuits and gravy`, '../locals-guide/black-honey-hashery.html', 4.75, ["coffee", "de pere", "black honey hashery"]);
 
 const blackSheep = new lgReview(`Black Sheep Pub & Grill`, '../img/locals-guide/eat/lunch/black-sheep-pub-and-grill.jpg', `Black Sheep burger and fries`, '../locals-guide/basils-ii.html', 4.25, ["alcohol", "green bay", "black sheep pub and grill", "black sheep pub & grill"]);
 
 const brickhouseBurgers = new lgReview(`Brickhouse Craft Burgers`, '../img/locals-guide/eat/lunch/brickhouse-craft-burgers.jpg', `Brickhouse cheese burger with fries`, '../locals-guide/brickhouse-craft-burgers.html', 4.0, ["alcohol", "de pere", "brickhouse burgers", "brick house"]);
 
-const copperState = new lgReview(`Copper State Brewing Co.`, '../img/locals-guide/eat/lunch/copper-state-brewing-burger.jpg', `Copper State Brewing macaroni and cheese burger with fries`, '../locals-guide/copper-state-brewing.html', 4.4, ["alcohol", "coffee", "green bay", "copperstate brewing", "copper state brewing"]);
+const copperState = new lgReview(`Copper State Brewing Co.`, '../img/locals-guide/eat/lunch/copper-state-brewing.jpg', `Copper State Brewing Co store front`, '../locals-guide/copper-state-brewing.html', 4.4, ["alcohol", "coffee", "green bay", "copperstate brewing", "copper state brewing"]);
 
 const cozumel = new lgReview(`Cozumel`, '../img/locals-guide/eat/lunch/cozumel.jpg', `Cozumel enchiladas`, '../locals-guide/cozumel.html', 4.3, ["alcohol", "green bay", "cozumel", "mexican"]);
 
@@ -50,6 +50,8 @@ const graystoneAleHouse = new lgReview(`Graystone Ale House`, '../img/locals-gui
 const greenBayDistillery = new lgReview(`Green Bay Distillery`, '../img/locals-guide/eat/lunch/green-bay-distillery.jpg', `Green Bay Distillery curd burger`, '../locals-guide/green-bay-distillery.html', 4.25, ["alcohol", "green bay", "green bay distillery", "distillery"]);
 
 const hagemeisterPark = new lgReview(`Hagemeister Park`, '../img/locals-guide/eat/lunch/hagemeister-park.jpg', `Hagemeister Park chicken carbonara`, '../locals-guide/hagemeister-park.html', 4.3, ["alcohol", "green bay", "hagemeister park"]);
+
+const kavarna = new lgReview(`Kavarna Coffeehouse`, '../img/locals-guide/drink/coffee/kavarna-coffeehouse.jpg', `Kavarna Coffeehouse bagged coffee`, '../locals-guide/kavarna-coffeehouse.html', 4.3, ["coffee", "green bay", "kavarna coffeehouse"]);
 
 const krolls = new lgReview(`Kroll's`, '../img/locals-guide/eat/lunch/krolls-west.jpg', `Kroll's cheesecurds`, '../locals-guide/krolls.html', 3.75, ["alcohol", "krolls west", "kroll's west"]);
 
@@ -85,9 +87,9 @@ const theAbbey = new lgReview(`The Abbey`, '../img/locals-guide/eat/lunch/the-ab
 
 const theBar = new lgReview(`The Bar`, '../img/locals-guide/eat/lunch/the-bar.jpg', `The Bar's buffalo wings`, '../locals-guide/the-bar.html', 4.0, ["alcohol", "green bay", "the bar"]);
 
-const theExchange = new lgReview(`The Exchange`, '../img/locals-guide/drink/coffee/the-exchange.jpg', `The Exchange coffee flight`,'../locals-guide/the-exchange.html', 4.15, ["coffee", "the exchange", "de pere"]);
+const theExchange = new lgReview(`The Exchange`, '../img/locals-guide/drink/coffee/the-exchange.jpg', `The Exchange coffee flight`,'../locals-guide/the-exchange.html', 4.1, ["coffee", "the exchange", "de pere"]);
 
-const content = [_1919Kitchen, anduzzis, basils, blackHoney, blackSheep, brickhouseBurgers, copperState, cozumel, dailyBuzz, driftInn, elMaya, elSarape, fourWayBar, glassNickelPizza, graystoneAleHouse, greenBayDistillery, hagemeisterPark, krolls, legendLarrys, luigisBistro, luigis, lunaCoffee, narrowBridge, nicoletCafe, notByBreadAlone, oldMexico, rAndDs, rustique, sammysPizza, sgambatis, stadiumView, stirUpsBar, theAbbey, theBar, theExchange]
+const content = [_1919Kitchen, anduzzis, basils, blackHoney, blackSheep, brickhouseBurgers, copperState, cozumel, dailyBuzz, driftInn, elMaya, elSarape, fourWayBar, glassNickelPizza, graystoneAleHouse, greenBayDistillery, hagemeisterPark, kavarna, krolls, legendLarrys, luigisBistro, luigis, lunaCoffee, narrowBridge, nicoletCafe, notByBreadAlone, oldMexico, rAndDs, rustique, sammysPizza, sgambatis, stadiumView, stirUpsBar, theAbbey, theBar, theExchange]
 
 // Generate initial content
 createLocalsGuide(content)
